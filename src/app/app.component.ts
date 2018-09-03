cea935a46424d7f31702c40ccbf61b97508bb12d
Ravi Shankar Kota <ravishankarkota@gmail.com>
2018-09-03 16:09:56 +0530

    headers navigated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  currentFeatureSelected: string = 'recipe';
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    //console.log(this.currentFeaureSelected);
+  }
+
+  headerlinkChanged(feature:string) {
+    console.log(feature);
+    this.currentFeatureSelected = feature;
+    //this.currentFeaureSelected = 'shopping';
+  }
 }
